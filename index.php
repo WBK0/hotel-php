22d70243f8e1aa04ddad375c86a2527de26af688
Bartlomiej <worldofworld0@gmail.com>
2023-10-11 11:42:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sideNavbar">
+            <div class="logo">
+                <image src="image/image.png" width="30px" height="40px" alt="logo"/>
+                <h2 class="companyName">Novotel</h2>
+            </div>
+            <div class="nav">
+
+            </div>
+        </div>
+        <div class="content">
+            <?php
+                $aha = "dasasd";
+                include('./components/navbar.php');
+            ?>
+        </div>
+        
+    </div>
+    
+    
+</body>
+</html>
